915b90d14fabbb65d1d37dd54623a6c9ddf7967b
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-27 21:54:28 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author
 
   attr_accessor :name, :author
-    
+
   def initialize(name)
     @name = name
     @post = []
@@ -20,7 +20,7 @@ class Author
   end
 
   def self.post_count
-    
+
   end
 
 
